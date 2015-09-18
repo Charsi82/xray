cbfc0bfe3459d4f4ad42911914786014dc0568d2
Charsi82 <charsi2011@gmail.com>
2015-09-18 16:55:23 +0300

    Create build_config_defines.h

---
+++ cs/build_config_defines.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// звуки включения\выключения фонарика
+#define TORCH_SOUNDS
+
+//изменение названия пункта меню юзания
+#define PROPBOX_USE_ITEMS_EXT	
+
+//изменение названия пункта меню аттача\детача оружейных аддонов
+#define PROPBOX_WPN_ADDONS_EXT	
+
+// стоимость пачки патронов
+#define COST_AMMO
+
+// рандом музыки в главном меню
+#define RND_MM_SOUND
+
+// нумерация фраз ответов как в ЗП
+#define NUM_PHRASES
+
+// скрытие/показ худа кнопками numpad+ numpad-
+#define SHOW_HIDE_HUD
+
+// область для выкидывания предметов
+#define DRAG_DROP_TRASH
+
+// панель артефактов
+#define AF_PANEL
+
+// запрет на продажу предметов с низкой кондицией
+#define NO_TRADE_TRASH
+
+// описание отказа в продаже в UIItemInfo (в разработке)
+#define TRADE_TIP
