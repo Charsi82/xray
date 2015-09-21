5ef55c41f4414ace3f265149d6972cd067940a88
Charsi82 <charsi2011@gmail.com>
2015-09-21 12:57:33 +0300

    Update holder_custom.cpp
    car\mgun attach\detach callbacks

--- cs/engine/xrGame/holder_custom.cpp
+++ cs/engine/xrGame/holder_custom.cpp
@@ -1,17 +1,24 @@
 #include "stdafx.h"
 #include "holder_custom.h"
+#include "pch_script.h"
+#include "script_callback_ex.h"
+#include "script_game_object.h"
+#include "game_object_space.h"
 #include "Actor.h"
 
 bool CHolderCustom::attach_Actor(CGameObject* actor)
 {
 	m_owner						= actor;
 	m_ownerActor				= smart_cast<CActor*>(actor);
-
+	CObject* pObj = smart_cast<CObject*>(this);
+	Actor()->callback(GameObject::eAttachVehicle)(pObj->ID());
 	return true;
 }
 
 void CHolderCustom::detach_Actor()
 {
+	CObject* pObj = smart_cast<CObject*>(this);
+	Actor()->callback(GameObject::eDetachVehicle)(pObj->ID());
 	m_owner						= NULL;
 	m_ownerActor				= NULL;
 }
